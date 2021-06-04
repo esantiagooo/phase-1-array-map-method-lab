2c72429bb6efd2a4c5436392f81d7bdbdeca75fe
esantiagooo <77802371+esantiagooo@users.noreply.github.com>
2021-06-03 21:28:52 -0400

    commit tree 8f454e3099002b88858461543607e4c72a710e50

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const tutorials = [
-  'what does the this keyword mean?',
+  'What does the this keyword mean?',
   'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
+  'Implementing Blockchain Web API',
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
   'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'What is the difference between == and ===?',
+  'What is the difference between event capturing and bubbling?',
+  'What is JSONP?'
 ];
+let tutorialsCapitalized =[];
 
 const titleCased = () => {
-  return tutorials
+  tutorials.map(function titleCased(stringVariable){
+
+    let newArr = [];
+
+    function stringToArray(string){
+      newArr = string.split(" ");
+    };
+    stringToArray(stringVariable);
+
+    const capitalized = newArr.map(function(string){
+      return string.charAt(0).toUpperCase()+string.slice(1);
+    })
+
+    const modfiedString = capitalized.join(" ");
+    tutorialsCapitalized.push(modfiedString);
+    console.log(modfiedString);
+  })
+
+  return tutorialsCapitalized;
 }
